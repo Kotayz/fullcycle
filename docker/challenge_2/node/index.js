bdb09f82b6e5451adeb501b96840f3744abd7d16
elson.dreveck <elson.alper@gmail.com>
2023-02-24 17:05:11 -0300

    ajuste para incluir registros ao subir app

--- docker/challenge_2/node/index.js
+++ docker/challenge_2/node/index.js
@@ -10,12 +10,32 @@ const config = {
 const mysql = require('mysql')
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Elson')`
-connection.query(sql)
-connection.end()
+const dropTable = `DROP TABLE IF EXISTS people`
+connection.query(dropTable)
+
+const createTable = `CREATE TABLE people(id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY(id))`
+connection.query(createTable)
+
+const insert = `INSERT INTO people(name) values('Sophia'), ('Scarlett'), ('Stella'), ('Sebastian'), ('Samuel'), ('Santiago')`
+connection.query(insert)
 
 app.get('/', (req, res) => {
-  res.send('<h1>Full Cycle Rocks!</h1>')
+  var message = '<h1>Full Cycle Rocks!</h1>'
+
+  connection.query('SELECT * FROM people', function (error, results, fields) {
+    if (error) throw error;
+
+    Object.keys(results).forEach(function(key) {
+      var row = results[key];
+      message = message + '<h2>' + row.name + '</h2>'
+      //console.log(row.name)
+    });
+
+    res.send(message)
+  })
+ 
+  
+  connection.end()
 })
 
 app.listen(port, () => {
